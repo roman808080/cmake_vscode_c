4ebede271af024d927eedbafce147867967b9e81
Roman Herasymenko <roman.herasymenko@gmail.com>
2022-03-12 20:04:49 +0100

    Playing with constructors and destructors.

--- src/experiments.cpp
+++ src/experiments.cpp
@@ -1,6 +1,40 @@
+#include <iostream>
+#include <memory>
 
+class BaseClass
+{
+public:
+    BaseClass()
+    {
+        std::cout << "It is BaseClass" << std::endl;
+    }
+
+    virtual ~BaseClass()
+    {
+        std::cout << "It is ~BaseClass" << std::endl;
+    }
+};
+
+class DerrivedClass : public BaseClass
+{
+public:
+    DerrivedClass()
+    {
+        std::cout << "It is DerrivedClass" << std::endl;
+    }
+
+    virtual ~DerrivedClass()
+    {
+        std::cout << "It is ~DerrivedClass" << std::endl;
+    }
+};
 
 int main()
 {
+    {
+        std::unique_ptr<BaseClass> object(new DerrivedClass);
+    }
+
+    std::cout << "Hello world!" << std::endl;
     return 0;
 }
