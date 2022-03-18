3802d4951d517dc3d9223d884ac674304fd11380
Roman Herasymenko <roman.herasymenko@gmail.com>
2022-03-18 19:58:27 +0100

    Adding a lot of code for tags.

--- src/tags.cpp
+++ src/tags.cpp
@@ -4,37 +4,71 @@
 #include <iostream>
 #include <algorithm>
 #include <bits/stdc++.h>
+
 using namespace std;
+
+namespace
+{
+    void read_hrml(std::vector<string> &hrml, const int number_of_strings)
+    {
+        for (int i = 0; i < number_of_strings; ++i)
+        {
+            std::string line("");
+            std::getline(std::cin, line);
+            hrml.push_back(line);
+        }
+    }
+
+    void read_queries(std::vector<string> &queries, const int number_of_queries)
+    {
+        for (int i = 0; i < number_of_queries; ++i)
+        {
+            std::string line("");
+            std::getline(cin, line);
+            queries.push_back(line);
+        }
+    }
+
+    void print_pair(const std::string &key, const std::string &value)
+    {
+        std::cout << "Key = " << key << ", value = " << value << std::endl;
+    }
+
+    void print_map_elements(const std::map<string, string> &map_of_hrml)
+    {
+        for (const auto &[key, value] : map_of_hrml)
+        {
+            print_pair(key, value);
+        }
+    }
+
+    void print_element(std::map<string, string> &map_of_hrml, const std::string &key)
+    {
+        print_pair(key, map_of_hrml[key]);
+    }
+
+}
 
 int main()
 {
-    int n, q, i;
-    cin >> n >> q;
+    int number_of_lines = 0;
+    int number_of_queries = 0;
 
-    string temp;
-    vector<string> hrml;
-    vector<string> quer;
-
+    cin >> number_of_lines >> number_of_queries;
     cin.ignore();
 
-    for (i = 0; i < n; i++)
-    {
-        getline(cin, temp);
-        hrml.push_back(temp);
-    }
+    std::vector<string> hrml;
+    std::vector<string> queries;
 
-    for (i = 0; i < q; i++)
-    {
-        getline(cin, temp);
-        quer.push_back(temp);
-    }
+    read_hrml(hrml, number_of_lines);
+    read_queries(queries, number_of_queries);
 
-    map<string, string> m;
-    vector<string> tag;
+    std::map<string, string> m;
+    std::vector<string> tag;
 
-    for (i = 0; i < n; i++)
+    for (const auto &line : hrml)
     {
-        temp = hrml[i];
+        std::string temp = line;
         temp.erase(remove(temp.begin(), temp.end(), '\"'), temp.end());
         temp.erase(remove(temp.begin(), temp.end(), '>'), temp.end());
 
@@ -60,26 +94,40 @@ int main()
             {
                 temp1 = t1;
             }
+
             tag.push_back(temp1);
-            m[*tag.rbegin() + "~" + p1] = v1;
+
+            auto key = *tag.rbegin() + "~" + p1;
+            m[key] = v1;
+
             while (ss)
             {
+                p1 = "";
+                v1 = "";
+
                 ss >> p1 >> ch >> v1;
-                m[*tag.rbegin() + "~" + p1] = v1;
+                if (p1 != "")
+                {
+                    key = *tag.rbegin() + "~" + p1;
+                    m[key] = v1;
+                }
             }
         }
     }
 
-    for (i = 0; i < q; i++)
+    for (const auto &query_line : queries)
     {
-        if (m.find(quer[i]) == m.end())
+        if (m.find(query_line) == m.end())
         {
             cout << "Not Found!\n";
         }
         else
         {
-            cout << m[quer[i]] << endl;
+            cout << m[query_line] << endl;
         }
     }
+
+    print_map_elements(m);
+
     return 0;
 }
